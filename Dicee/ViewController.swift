a1aee68db56f7d7946c5d4d2075845e55df336f0
Juri Plugatariov <juri.plugatariov@heathmont.net>
2018-06-15 15:38:04 +0300

    Implemented dice app

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
+    @IBOutlet weak var leftDice: UIImageView!
+    @IBOutlet weak var rightDice: UIImageView!
+    @IBOutlet weak var rollButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateDiceImages()
     }
 
-
+    @IBAction func rollDice(_ sender: UIButton) {
+        updateDiceImages()
+    }
+    
+    func setRandomDice(diceImage: UIImageView, index: Int) {
+        diceImage.image = UIImage(named: self.diceArray[index])
+    }
+    
+    func updateDiceImages() {
+        for imageView in [self.leftDice, self.rightDice] {
+            let index = Int.random(in: 1 ..< 6)
+            
+            setRandomDice(diceImage: imageView!, index: index)
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        updateDiceImages()
+    }
+    
 }
 
---
+++ README.md
@@ -0,0 +1,3 @@
+### Dicee
+
+Simple dice app to play with new swift 4.2 features 
